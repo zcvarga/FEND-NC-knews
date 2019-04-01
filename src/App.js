785f613a1db200dd5a8c702cb47960db3365566e
Zsuzsi Varga <zsuzsivarga@Zsuzsis-MacBook-Pro.local>
2019-04-01 11:03:14 +0100

    React app created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Heading from './components/Heading'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Heading />
+      </div>
+    );
+  }
+}
+
+export default App;
